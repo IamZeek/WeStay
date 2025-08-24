d24b43f1167aa1c86c40a4f0eeea23e45378329e
Zeerak Kafeel <Zeerakk30@gmail.com>
2025-08-24 07:09:15 +0500

    Notification bug fixes

--- WeStay.NotificationService/Services/Interfaces/ISMSService.cs
+++ WeStay.NotificationService/Services/Interfaces/ISMSService.cs
@@ -7,5 +7,8 @@ namespace WeStay.NotificationService.Services.Interfaces
         Task<bool> SendSMSAsync(string toPhoneNumber, string message);
         Task<bool> SendTemplatedSMSAsync(string toPhoneNumber, string templateName, Dictionary<string, string> variables);
         Task<bool> SendBookingConfirmationSMSAsync(string toPhoneNumber, string bookingCode, DateTime checkInDate);
+        Task<bool> SendVerificationSMSAsync(string toPhoneNumber, string verificationCode);
+        Task<bool> ValidatePhoneNumberAsync(string phoneNumber);
+        Task<string> FormatPhoneNumberAsync(string phoneNumber);
     }
 }
